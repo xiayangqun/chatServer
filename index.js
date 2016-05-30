06a2048d693d294a557a1eccd34b56d59776a85a
xiayangqun <xiayangqun@catcap.cn>
2016-05-30 10:52:53 +0800

    服务端解析客户端传回来的json完毕

--- index.js
+++ index.js
@@ -11,8 +11,21 @@ var webSocketServer=ws.createServer({
 webSocketServer.on("connection",function(connectHandle) {
     
     connectHandle.on("message",function(data) {
-        console.log("we get data from client: " + data);
-        connectHandle.send(data+"from server!!");
+        
+        //console.log('we get data from client: ' + data);
+
+        var commend="chat";
+        var clientData=JSON.parse(data);
+        var serverData={
+            time: Date.parse(new Date())/1000 ,
+            world:  'we recvied data :'+clientData.world,
+        };
+        var responseData={
+            clientData:clientData,
+            serverData:serverData,
+            commend:commend
+        };
+        connectHandle.send(JSON.stringify(responseData)) ;
     })
 
     connectHandle.on("close",function(connectHandle){
